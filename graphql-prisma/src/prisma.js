a2e54f8a56d3f315fb6b8d2c45d904e4868c165f
Bardh Osmani <bardhiosmani01@gmail.com>
2022-10-02 19:11:11 +0200

    Using async/await for mutations/query & checking if data exists

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -2,5 +2,60 @@ import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'localhost:4466'
+    endpoint: 'http://localhost:4466'
 })
+
+const createPostForUser = async (authorId, data) => {
+    const userExist = await prisma.exists.User({id: authorId})
+
+    if(!userExist){
+        throw new Error('User not found')
+    }
+
+    const post = await prisma.mutation.createPost({
+        data: {
+            ...data,
+            author: {
+                connect: {
+                    id: authorId
+                }
+            }
+        }
+    }, '{ id }')
+    
+    return post.author 
+}
+
+const updatePostForUser = async (postId, data) => {
+
+    const postExist = await prisma.exists.Post({id: postId})
+
+    if(!postExist){
+        throw new Error('Post not found')
+    }
+
+    const post = await prisma.mutation.updatePost({
+        data: {
+            ...data
+        },
+        where: {
+            id: postId
+        }
+    },'{ id author { id } }')
+   
+    return post.author;
+}
+
+// updatePostForUser("112",{
+//     body: 'I mire eshte ky'
+// }).then((user)=>{
+//     console.log(user)
+// }).catch(err => {console.log(err)})
+
+// createPostForUser("cl8c40ixu00eo0998kxxdil4j", {
+//     title: 'Great books to read',
+//     body: 'The War of Art',
+//     published: true
+// }).then((user)=>{
+//     console.log(user)
+// })
